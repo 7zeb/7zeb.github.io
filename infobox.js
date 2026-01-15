156747803aad62bb5f2237df14bed304f99da847
7Zeb <66693249+7zeb@users.noreply.github.com>
2026-01-15 08:22:47 -0600

    update the info box to current information

--- infobox.js
+++ infobox.js
@@ -5,16 +5,15 @@ document.addEventListener("DOMContentLoaded", function () {
     <h3>Projects 7Zeb is Working On</h3>
     <ul style="margin-left:1em;">
     <li>Windows 8 Upgrade Saga</li>
-    <li>Getting Windows 7 on the Dell Inspiron 5566</li>
-    <li>RVE (Rutwik Video Editor)</li>
-    <li>Windows 1.0 Development History</li>
+    <li>WasmForge</li>
+    <li>Exploring Videos</li>
+    <li>Unboxing Videos</li>
     <br>
     <h3>Upcoming 7Zeb Events</h3>
     <p style="margin-left:0.5em;">These are a list of some upcoming 7Zeb Events</p>
     <ul style="margin-left:1em;">
-      <li>Windows 1.0's 40th birthday (November 20, 2025)</li>
-      <li>2nd Fall Upload Boom (November 21 - November 30)</li>
-      <li>2nd Winter Upload Boom (December 19 - January 5)</li>
+      <li>Windows Vista's 19th Birthday (January 30, 2026)</li>
+      <li>2nd Spring Upload Boom (March 6 - March 15, 2026)</li>
     </ul>
   `;
 });
