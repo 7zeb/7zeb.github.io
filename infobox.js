574a7f735726e9e1e6a28808c0427c7b321c55d9
7Zeb <66693249+7zeb@users.noreply.github.com>
2026-01-22 13:25:55 -0600

    update the info box since something cool is happening tomorrow

--- infobox.js
+++ infobox.js
@@ -12,6 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
     <h3>Upcoming 7Zeb Events</h3>
     <p style="margin-left:0.5em;">These are a list of some upcoming 7Zeb Events</p>
     <ul style="margin-left:1em;">
+      <li>Windows Upgrade Saga Speedrun Week 2 (January 23, 2026)</li>
       <li>Windows Vista's 19th Birthday (January 30, 2026)</li>
       <li>2nd Spring Upload Boom (March 6 - March 15, 2026)</li>
     </ul>
